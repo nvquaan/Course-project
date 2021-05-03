9340ddfbfa1cdd127eec41e07af5d917e4f38245
Nguyen Van Quan <kbsa2499@gmail.com>
2021-05-03 19:41:51 +0700

    3/5

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { MoviesService } from 'src/app/service/movies.service';
-import { TvService } from 'src/app/service/tv.service';
-import { delay } from 'rxjs/internal/operators/delay';
-import {CoursesService} from '../../service/courses.service';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { MoviesService } from "src/app/service/movies.service";
+import { TvService } from "src/app/service/tv.service";
+import { delay } from "rxjs/internal/operators/delay";
+import { CoursesService } from "../../service/courses.service";
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
   nowPlaying: any;
@@ -18,43 +18,58 @@ export class HomeComponent implements OnInit {
   constructor(
     private movies: MoviesService,
     private tv: TvService,
-    public courseSV: CoursesService,
+    public courseSV: CoursesService
   ) {
     this.responsiveOptions = [
       {
-        breakpoint: '1024px',
+        breakpoint: "1024px",
         numVisible: 3,
-        numScroll: 3
+        numScroll: 3,
       },
       {
-        breakpoint: '768px',
+        breakpoint: "768px",
         numVisible: 2,
-        numScroll: 2
+        numScroll: 2,
       },
       {
-        breakpoint: '560px',
+        breakpoint: "560px",
         numVisible: 1,
-        numScroll: 1
-      }
+        numScroll: 1,
+      },
     ];
-    this.courseSV.getAllCourses().subscribe(res => console.log(res));
-
+    this.courseSV.getAllCourses().subscribe((res) => console.log(res));
   }
   ngOnInit() {
     this.trendingMovies(1);
     this.tvShow(1);
   }
   trendingMovies(page: number) {
-    this.movies.getNowPlaying(page).pipe(delay(2000)).subscribe((res: any) => {
-      this.nowPlaying = res.results;
-      this.loader = false;
-    });
+    this.movies
+      .getNowPlaying(page)
+      .pipe(delay(2000))
+      .subscribe((res: any) => {
+        this.nowPlaying = res.results;
+        this.loader = false;
+      });
+    // this.courseSV.getAllCategories().subscribe(res => {
+    //   this.nowPlaying = res.data;
+    //   res.data.forEach(d => console.log(d.name));
+    // })
   }
 
   tvShow(page: number) {
-    this.tv.getTvOnTheAir(page).pipe(delay(2000)).subscribe((res: any) => {
-      this.tvShows = res.results;
-      this.loader = false;
-    });
+    this.tv
+      .getTvOnTheAir(page)
+      .pipe(delay(2000))
+      .subscribe((res: any) => {
+        this.tvShows = res.results;
+        this.loader = false;
+      });
+  }
+
+  getAllCategories(){
+      this.courseSV.getAllCategories().subscribe(res => {
+          
+      })
   }
 }
